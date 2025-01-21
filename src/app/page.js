fe27f1d0a6731fcf142091114a1c9ba9f4de8353
sujithm33 <sujthmksujith33@gmail.com>
2025-01-21 12:48:31 +0530

    second

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-dvh w-dvw ">sample</div>
+    <div className="flex items-center justify-center h-dvh w-dvw ">samplee</div>
   );
 }
