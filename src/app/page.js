b282cd13ba8bf6f69ebde919252cc729da0ae610
sujithm33 <sujthmksujith33@gmail.com>
2025-01-21 14:35:32 +0530

    sachin

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center h-dvh w-dvw ">
       shelba thayoli
+      <span className="text-base text-red-600 font-semibold">
+        koottu vilai sachin
+      </span>
+      <p className="text-base text-yellow-400 font-semibold">velumani</p>
     </div>
   );
 }
