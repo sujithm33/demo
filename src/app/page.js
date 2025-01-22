addf4a59bbf08cce9209909f3e5b2fd2e67dda5c
sujithm33 <sujthmksujith33@gmail.com>
2025-01-22 10:34:25 +0530

    photoupdate

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,15 @@
 import Image from "next/image";
 import sachin from "@/Assets/sachin.png";
+import sachin1 from "@/Assets/sachin2.jpg";
+import sachin2 from "@/Assets/sachin3.jpg";
+import sachin3 from "@/Assets/sachin4.jpg";
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-dvh w-dvw ">
-      <div>
-        shelba thayoli
-        <span className="text-base text-red-600 font-semibold">
-          koottu vilai sachin
-        </span>
-        <p className="text-base text-yellow-400 font-semibold">velumani</p>
-      </div>
+    <div className="flex flex-col  min-h-dvh w-dvw ">
+      <h1 className="py-5 font-bold text-5xl text-center bg-black text-white">
+        VADAKAN Sachin
+      </h1>
       <Hero />
     </div>
   );
@@ -17,7 +17,7 @@ export default function Home() {
 
 export function Hero() {
   return (
-    <div className="bg-white p-5 rounded-lg text-black flex items-end gap-2">
+    <div className="bg-white p-5 rounded-lg text-black flex flex-col items-center gap-3 gap-2 ">
       <Image
         src={sachin}
         width={200}
@@ -25,10 +25,27 @@ export function Hero() {
         alt="sachin"
         className="aspect-square rounded-lg"
       />
-      <div>
-        <p className="font-semibold text-3xl">Sachin</p>
-        <span className="text-base font-semibold">The VADAKAN</span>
-      </div>
+      <Image
+        src={sachin1}
+        width={200}
+        height={200}
+        alt="sachin"
+        className="aspect-square rounded-lg"
+      />
+      <Image
+        src={sachin2}
+        width={200}
+        height={200}
+        alt="sachin"
+        className="aspect-square rounded-lg"
+      />
+      <Image
+        src={sachin3}
+        width={200}
+        height={200}
+        alt="sachin"
+        className="aspect-square rounded-lg"
+      />
     </div>
   );
 }
